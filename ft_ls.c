49ee9fe66d9a73bfbaff5f9be456b155a1130fe2
Vincent Arnaud <varnaud@localhost.localdomain>
2016-12-12 07:52:32 -0800

    I hope you don't mind

---
+++ ft_ls.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/12 02:17:54 by varnaud           #+#    #+#             */
+/*   Updated: 2016/12/12 07:51:21 by varnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <dirent.h> 
+#include <stdio.h> 
+#include "libft.h"
+#include "ft_ls.h"
+
+void	set_flags(char *arg, int *flags)
+{
+	while (*arg)
+	{
+		if (*arg == 'a')
+		{
+			*flags |= FLAG_SMALL_A;
+		}
+		else if (*arg == 'l')
+		{
+			*flags |= FLAG_SMALL_L;
+		}
+		else if (*arg == 'r')
+		{
+			*flags |= FLAG_SMALL_R;
+		}
+		else if (*arg == 'R')
+		{
+			*flags |= FLAG_R;
+		}
+		else if (*arg == 't')
+		{
+			*flags |= FLAG_SMALL_T;
+		}
+		arg++;
+	}
+}
+
+void	display_flags(int flags)
+{
+	ft_putstr("Flags: ");
+	if (flags & FLAG_SMALL_A)
+		ft_putchar('a');
+	if (flags & FLAG_SMALL_L)
+		ft_putchar('l');
+	if (flags & FLAG_SMALL_R)
+		ft_putchar('r');
+	if (flags & FLAG_R)
+		ft_putchar('R');
+	if (flags & FLAG_SMALL_T)
+		ft_putchar('t');
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	DIR				*d;
+	struct dirent	*dir;
+	int				flags;
+	char			*directory;
+
+	directory = NULL;
+	flags = 0;
+	while (*++argv)
+	{
+		if (**argv == '-')
+			set_flags((*argv + 1), &flags);
+		else
+			directory = ft_strdup(*argv);
+	}
+	display_flags(flags);
+	d = opendir(directory == NULL ? CUR_DIR : directory);
+	if (d)
+	{
+		while ((dir = readdir(d)) != NULL)
+		{
+			ft_putendl(dir->d_name);
+		}
+		closedir(d);
+	}
+	return(0);
+}
---
+++ ft_ls.h
@@ -0,0 +1,10 @@
+#ifndef FT_LS_H
+# define FT_LS_H
+# define FLAG_SMALL_A 1
+# define FLAG_SMALL_L 2
+# define FLAG_SMALL_R 4
+# define FLAG_R 8
+# define FLAG_SMALL_T 16
+# define CUR_DIR "."
+
+#endif
