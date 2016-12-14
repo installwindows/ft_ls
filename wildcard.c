bafa2bb35055c24e011d9fac973a510d4a217414
Vincent Arnaud <varnaud@localhost.localdomain>
2016-12-13 21:05:57 -0800

    ayy

--- wildcard.c
+++ wildcard.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-// Function that matches input str with
-// given wildcard pattern
+
 int		strmatch(char *str, char *pattern, int n, int m)
 {
 	int		i;
 	int		j;
-    // empty pattern can only match with
-    // empty string
+    int		lookup[n + 1][m + 1];
+
     if (m == 0)
         return (n == 0);
-    // lookup table for storing results of
-    // subproblems
-    int		lookup[n + 1][m + 1];
-    // initailze lookup table to false
     memset(lookup, 0, sizeof(lookup));
-
-    // empty pattern can match with empty string
     lookup[0][0] = 1;
- 
-    // Only '*' can match with empty string
     j = 1;
 	while (j <= m)
 	{
@@ -28,38 +19,22 @@ int		strmatch(char *str, char *pattern, int n, int m)
             lookup[0][j] = lookup[0][j - 1];
 		j++;
 	}
- 
-    // fill the table in bottom-up fashion
 	i = 1;
     while (i <= n)
     {
 		j= 1;
         while (j <= m)
         {
-            // Two cases if we see a '*'
-            // a) We ignore ‘*’ character and move
-            //    to next  character in the pattern,
-            //     i.e., ‘*’ indicates an empty sequence.
-            // b) '*' character matches with ith
-            //     character in input
             if (pattern[j - 1] == '*')
                 lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
- 
-            // Current characters are considered as
-            // matching in two cases
-            // (a) current character of pattern is '?'
-            // (b) characters actually match
             else if (pattern[j - 1] == '?' || str[i - 1] == pattern[j - 1])
                 lookup[i][j] = lookup[i - 1][j - 1];
- 
-            // If characters don't match
             else
 				lookup[i][j] = 1;
 			j++;
         }
 		i++;
-    }
- 
+    } 
     return lookup[n][m];
 }
  
